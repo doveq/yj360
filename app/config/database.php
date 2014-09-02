cf310c0fc6c7f09a4ba2a81b876f5e4d9895a065
Andy Chen <nine52seven@gmail.com>
2014-09-02 17:30:45 +0800

    update mysql passed

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'yj360',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'info.works',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
