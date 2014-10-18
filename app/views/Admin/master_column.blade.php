5c7756e260df55a63aa47b7df54baed5fcb91de9
Andy Chen <nine52seven@gmail.com>
2014-10-18 14:54:58 +0800

    update

--- app/views/Admin/master_column.blade.php
+++ app/views/Admin/master_column.blade.php
@@ -41,7 +41,7 @@
                   <li><a href="/admin/favorite">我的收藏</a></li>
                   <li class="divider"></li>
                   <li><a href="/admin/uploadbank">上传题库</a></li>
-                  <li><a href="/admin/training">教室训练集</a></li>
+                  <li><a href="/admin/training">教师训练集</a></li>
 
                 </ul>
               </li>
