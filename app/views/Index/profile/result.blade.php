21ea0ac0360e3cd2d8019705d72fcef84a07990b
Lin <3094047@qq.com>
2014-10-27 23:48:36 +0800

    Signed-off-by: Lin <3094047@qq.com>

--- app/views/Index/profile/result.blade.php
+++ app/views/Index/profile/result.blade.php
@@ -18,7 +18,7 @@
           @foreach($list as $k => $v)
             <tr>
                 <td class="tytd">
-                  @if(empty($v->question))
+                  @if(empty($v->question->txt))
                     该题已下架
                   @else
                     <a href="/topic?id={{$v->question->id}}" target="_blank">{{$v->question->txt}}</a>
