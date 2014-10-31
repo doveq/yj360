4f477534a2500b85ee1417fca37b5e0edfc702bf
Lin <3094047@qq.com>
2014-10-31 17:34:00 +0800

    Signed-off-by: Lin <3094047@qq.com>

--- app/controllers/Admin/ExamPaperController.php
+++ app/controllers/Admin/ExamPaperController.php
@@ -1,17 +1,25 @@
 <?php namespace Admin;
 use View;
 use Session;
+use Input;
+use Column;
 
 class ExamPaperController extends \BaseController {
 
 	public function index()
 	{
+
 		return $this->adminView('index');
 	}
 
 	public function showAdd()
 	{
-		return $this->adminView('examPaper.add');
+		$columnId = Input::get('column_id');
+
+		$parent = Column::find($columnId);
+        $paths = array_reverse($parent->getPath($parent->id));
+
+		return $this->adminView('examPaper.add', compact('parent', 'paths', 'columnId') );
 	}
 
 }
--- app/views/Admin/examPaper/add.blade.php
+++ app/views/Admin/examPaper/add.blade.php
@@ -1,13 +1,19 @@
 @extends('Admin.master_column')
-@section('title')科目管理@stop
+@section('title')试卷管理@stop
 
 
 @section('content')
+
   <div class="row">
     <ol class="breadcrumb">
+      <li>{{link_to_route('admin.column.index', '科目管理')}}</li>
+        @foreach ($paths as $key => $path)
+        <li>{{link_to_route('admin.column.index', $path['name'], array('parent_id' => $path['id']))}}</li>
+        @endforeach
       <li class="active">添加试卷</li>
     </ol>
   </div>
+
   <div class="row">
       <form method="POST" action="/admin/examPaper/doAdd" role="form" class="form-horizontal">
         <div class="form-group">
@@ -18,7 +24,7 @@
         </div>
 
         <div class="form-group">
-          <label for="sort_name" class="col-md-2 control-label">试卷名</label>
+          <label for="sort_name" class="col-md-2 control-label">价格</label>
           <div class="col-md-6">
             <input class="form-control" id="title" name="title" type="text" value="">
           </div>
