2c14a9c42e0155928f481a1441e84f11b31de010
Lin <3094047@qq.com>
2014-11-08 15:34:45 +0800

    Signed-off-by: Lin <3094047@qq.com>

--- app/views/Index/column/2.blade.php
+++ app/views/Index/column/2.blade.php
@@ -16,17 +16,25 @@
       <div class="clear"></div>
 
       <div class="classes-list">
-          @foreach ($content as $list)
-          <div class="classse-box" style="height:120px;width:320px; border-top:0;background-color:{{$list->bgcolor}}">
-            <div class="classes-txt" style="background-color:{{$list->bgcolor}};vertical-align:middle;">
-              <a href="/topic?exam={{$list->id}}" style="color:#fff;">
-              <div><h2><b>{{$list->title}}</b></h2></div>
-              </a>
-            </div>
-          </div>
-          @endforeach
+          <table class="table-2" border="0" cellpadding="0" cellspacing="0">
+         
+            @if(!empty($content))
+              @foreach($content as $list)
+                <tr>
+                    <td class="tytd">
+                        <a href="/topic?exam={{$list->id}}" target="_blank">{{$list->title}}</a>
+                    </td>
+                </tr>
+                <tr><td colspan="2">
+                    <div class="table-2-sp"></div>
+                </td></tr>
+              @endforeach
+            @endif
+          </table>
+
           <div class="clear"></div>
       </div>
+
   </div>
   <div class="clear"></div>
 </div> <!-- /container -->
