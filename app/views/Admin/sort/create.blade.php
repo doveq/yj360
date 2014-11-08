fe59b196a9c669620041c25fab790597a94db565
Andy Chen <nine52seven@gmail.com>
2014-11-08 14:21:15 +0800

    update

--- app/views/Admin/sort/create.blade.php
+++ app/views/Admin/sort/create.blade.php
@@ -11,7 +11,7 @@
       <li>{{link_to_route('admin.sort.index', '题目分类管理')}}</li>
       @if ($query['parent_id'] > 0)
         @foreach ($paths as $key => $path)
-        <li>{{link_to_route('admin.column.index', $path['name'], array('parent_id' => $path['id']))}}</li>
+        <li>{{link_to_route('admin.sort.index', $path['name'], array('parent_id' => $path['id']))}}</li>
         @endforeach
       @endif
       <li class="active">添加分类</li>
