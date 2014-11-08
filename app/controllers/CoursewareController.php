2cc924c7f48ac21f0c7c811c88c668b5962dc4b1
Andy Chen <nine52seven@gmail.com>
2014-11-08 20:39:13 +0800

    update courseware

--- app/controllers/CoursewareController.php
+++ app/controllers/CoursewareController.php
@@ -11,7 +11,25 @@ class CoursewareController extends BaseController
 	/* 登录处理 */
 	public function index()
 	{
-        return $this->indexView('courseware.index');
+        $query = Input::only('column_id', 'd1');
+        $columns = Column::find($query['column_id'])->child()->whereStatus(1)->orderBy('ordern', 'ASC')->get();
+        // dd($columns);
+        // dd(public_path() . '/data/flash_exe/info.php');
+        include_once public_path() . '/data/flash_exe/info.php';
+
+        $lists = array();
+        if (!isset($query['d1'])) {
+            // foreach ($dir_info as $key => $value) {
+            //     $lists[$key] = array('name' => $value['name']);
+            // }
+            $lists = $dir_info;
+        } else {
+            // foreach ($dir_info[$query['d1']]['files'] as $key => $value) {
+            //     $lists[$key] = array('name' => $value['name']);
+            // }
+            $lists = $dir_info[$query['d1']];
+        }
+        return $this->indexView('courseware.index', compact('columns', 'query', 'lists'));
 	}
 
 }
