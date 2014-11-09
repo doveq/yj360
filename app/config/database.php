1c926f7a82f83e054944d29e3813fb2c4773b96f
Lin <3094047@qq.com>
2014-11-09 21:29:49 +0800

    Signed-off-by: Lin <3094047@qq.com>

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '182.92.223.66',
+			'host'      => '127.0.0.1',
 			'database'  => 'yj360',
 			'username'  => 'root',
 			'password'  => 'info.works',
