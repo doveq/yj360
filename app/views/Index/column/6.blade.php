6f544c5d994d441d52561a1c34752ea416fef871
zhouds <594048700@qq.com>
2014-12-29 10:29:36 +0800

    Signed-off-by: zhouds <594048700@qq.com>

--- app/views/Index/column/6.blade.php
+++ app/views/Index/column/6.blade.php
@@ -20,16 +20,20 @@
             @if(!empty($content))
               @foreach($content as $list)
                 <tr>
-                    <td class="tytd">
+                    <td class="cl tytd" style="min-width:400px;display:inline-block;">
                         <!--
                         <a href="/topic?exam={{$list->exam_id}}&column={{$column->id}}" target="_blank">{{$list->exam->title}}</a>
                         -->
-                        {{$list->exam->title}}
-                    </td>
-                    <td>
-                        <a href="/topic?exam={{$list->exam_id}}&column={{$column->id}}" target="_blank" style="color:#428bca;">练习模式</a>
+                        <span style="float:left;margin-left:-10px;">
+                            <li style="color:#00B1BC;"><span style="color:#000;">{{$list->exam->title}}</span></li>
+                        </span>
+                        
+                        <span style="float:right;">
                         &nbsp;&nbsp;
-                        <a href="/topic?exam={{$list->exam_id}}&column={{$column->id}}&real=1" target="_blank" style="color:#428bca;">考试模式</a>
+                        <a href="/topic?exam={{$list->exam_id}}&column={{$column->id}}" target="_blank" style="color:#00B1BC;">练习模式</a>
+                        &nbsp;&nbsp;
+                        <a href="/topic?exam={{$list->exam_id}}&column={{$column->id}}&real=1" target="_blank" style="color:#00B1BC;">考试模式</a>
+                        </span>
                     </td>
                 </tr>
                 <tr><td colspan="2">
--- app/views/Index/notice/show.blade.php
+++ app/views/Index/notice/show.blade.php
@@ -43,7 +43,9 @@
       </div>
 
       <div class="notice-comment">
-      	<div class="notice-comment-total">评论：（共{{count($info->commentcount)}}条）</div>
+      	<div class="notice-comment-total">评论：（共{{count($info->commentcount)}}条）
+      	    <span style="color:#F97E7E;font-weight:normal;">注：发布不友善言论或反动言论一律永久封号！严重者将追究法律责任！</span>
+      	</div>
       	
       	{{-- 评论form --}}
         <form method="post" action="/notice/doComment" style="margin-bottom:40px;">
